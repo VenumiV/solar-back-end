7c8c3990264afe153c9140555eaadfaa49649fb4
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-10-18 22:40:42 +0530

    error handling

--- src/domain/errors/error.ts
+++ src/domain/errors/error.ts
@@ -4,13 +4,6 @@ export class NotFoundError extends Error {
       this.name = "NotFoundError";
     }
   }
-  
-  export class BadRequestError extends Error {
-    constructor(message: string) {
-      super(message);
-      this.name = "BadRequestError";
-    }
-  }
 
 
   export class ValidationError extends Error {
