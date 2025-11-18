9fe5fb31dc64365841090042d001ed3ecca7bf33
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-11-18 23:56:12 +0530

    clerk- authentication and ngrok for online api link

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { loggerMiddleware } from "./api/middlewares/logger-middleware";
 import solarUnitRouter from "./api/solar-units";
 import { connectDB } from "./infrastructure/db";
 import cors from "cors";
+import webhooksRouter from "./api/webhooks";
 
 const server = express();
 server.use(express.json());
@@ -16,6 +17,8 @@ server.use(loggerMiddleware);
 
 server.use("/api/solar-units", solarUnitRouter);
 server.use("/api/energy-generation-records", energyGenerationRecordRouter);
+server.use("/api/webhooks",webhooksRouter);
+
 
 server.use(globalErrorHandler);
 
