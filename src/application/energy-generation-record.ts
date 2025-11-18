fbedf3267dfd9de866e1d0eab52d0b34117b657d
Venumi Alagoda <166162600+VenumiV@users.noreply.github.com>
2025-11-18 06:43:51 +0530

    Moved datachart shown logic to backend

--- src/application/energy-generation-record.ts
+++ src/application/energy-generation-record.ts
@@ -1,5 +1,7 @@
 import { EnergyGenerationRecord } from "../infrastructure/entities/EnergyGenerationRecord";
 import { NextFunction, Request, Response } from "express";
+import { GetAllEnergyGenerationRecordsQueryDto } from "../domain/dtos/solar-unit";
+import { ValidationError } from "../domain/errors/error";
 
 export const getAllEnergyGenerationRecordsBySolarUnitId = async (
   req: Request,
@@ -8,7 +10,12 @@ export const getAllEnergyGenerationRecordsBySolarUnitId = async (
 ) => {
   try {
     const { id } = req.params;
-    const { groupBy } = req.query;
+    const results = GetAllEnergyGenerationRecordsQueryDto.safeParse(req.query);
+    if (!results.success) {
+      throw new ValidationError(results.error.message);
+    }
+
+    const { groupBy, limit } = results.data;
 
     if (!groupBy) {
       const energyGenerationRecords = await EnergyGenerationRecord.find({
@@ -33,9 +40,14 @@ export const getAllEnergyGenerationRecordsBySolarUnitId = async (
           $sort: { "_id.date": -1 },
         },
       ]);
-      res.status(200).json(energyGenerationRecords);
-    }
-  } catch (error) {
+
+       if (!limit) {
+        res.status(200).json(energyGenerationRecords);
+        return;
+      }
+      res.status(200).json(energyGenerationRecords.slice(0, parseInt(limit)));
+  }
+}catch (error) {
     next(error);
   }
 };
